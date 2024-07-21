e36abe30639206f1489cac2613a32689af1c61d8
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2024-07-21 18:31:10 +0200

    rename: executable name on build

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "delete",
+        .name = "zix",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,

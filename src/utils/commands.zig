64fac28e33f0849cf3e589a76e92b3bf8929437d
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2024-07-22 12:49:02 +0200

    add: bullet in configPrint

--- src/utils/commands.zig
+++ src/utils/commands.zig
@@ -3,11 +3,11 @@ const style = @import("style.zig").Style;
 
 pub fn configPrint(repo: []const u8, update: []const u8, diff: []const u8, hostname: []const u8) void {
     std.debug.print(
-        \\ {s}repo{s} = {s}{s}{s}
-        \\ {s}hostname{s} = {s}{s}{s}
-        \\ {s}update{s} = {s}{s}{s}
-        \\ {s}diff{s} = {s}{s}{s}
-    , .{ style.Black, style.Reset, style.Cyan, repo, style.Reset, style.Black, style.Reset, style.Cyan, hostname, style.Reset, style.Black, style.Reset, style.Cyan, update, style.Reset, style.Black, style.Reset, style.Cyan, diff, style.Reset });
+        \\ {s}◉ repo{s} = {s}{s}{s}
+        \\ {s}◉ hostname{s} = {s}{s}{s}
+        \\ {s}◉ update{s} = {s}{s}{s}
+        \\ {s}◉ diff{s} = {s}{s}{s}
+    , .{ style.Red, style.Reset, style.Cyan, repo, style.Reset, style.Red, style.Reset, style.Cyan, hostname, style.Reset, style.Red, style.Reset, style.Cyan, update, style.Reset, style.Red, style.Reset, style.Cyan, diff, style.Reset });
 }
 pub const gitPullCmd = std.fmt.comptimePrint("git -C {s} pull", .{"~/.dotfiles"});
 pub const gitDiffCmd = std.fmt.comptimePrint("git -C {s} diff --exit-code", .{"~/.dotfiles"});

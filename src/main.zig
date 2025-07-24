b0283c84fdd57017f6d0a4816e504f9a9efbedcb
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-07-24 02:46:32 +0200

    fix(main): ensure leak detection runs even if app.init fails

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,14 @@
 const std = @import("std");
-const allocator = @import("allocator");
+const detectLeaks = @import("allocator").detectLeaks;
 const app = @import("app/init.zig");
 const builtin = @import("builtin");
 
 pub const VERSION = "dev";
 
 pub fn main() !void {
-    try app.init();
-    if (allocator.detectLeaks()) {
+    defer if (detectLeaks()) {
         std.posix.exit(1);
-    }
+    };
+
+    try app.init();
 }
